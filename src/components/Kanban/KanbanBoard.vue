6bf4a855531d32b538426612f271d91d0196c6d0
Bogdan Korać <bogdan.korac@gdi.net>
2025-03-24 14:36:37 +0100

    fix: added localization and fixes some dragging quirks

--- src/components/Kanban/KanbanBoard.vue
+++ src/components/Kanban/KanbanBoard.vue
@@ -4,11 +4,15 @@
     :disabled="isDraggingDisabled"
     item-key="id"
     tag="div"
+    :move="onMoveCallback"
     class="!ml-1 !mt-3 flex overflow-x-auto overflow-y-hidden" :group="{name: 'columns'}">
     <template #item="{element: column, index}">
-      <kanban-column v-if="index !== kanban.columns.length - 1" :column="column" :column-index="index" />
+      <kanban-column
+        v-if="index !== kanban.columns.length - 1" :column="column" :column-index="index"
+        @mouseleave="isDraggingDisabled = false" />
       <kanban-column-add
         v-else
+        @added-column="isDraggingDisabled = false"
         @mouseenter="isDraggingDisabled = true"
         @mouseleave="isDraggingDisabled = false" />
     </template>
@@ -24,6 +28,19 @@ import { ref } from 'vue'
 
 const { kanban } = useKanbanStore()
 const isDraggingDisabled = ref(false)
+
+//don't know how to get the type of this event
+//disabling dragging incase it's the dummy element being moved, or something being moved in its position
+function onMoveCallback(evt: any) {
+  if (evt.draggedContext.element.title === 'DummyElement') {
+    return false
+  } else if (evt.draggedContext.futureIndex === kanban.columns.length - 1) {
+    {
+      return false
+    }
+  }
+  return true
+}
 </script>
 
 
